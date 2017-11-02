b7112ac48cc09ca261241add5bd55c143d5f4217
dbaines <dbaines@gmail.com>
2017-11-02 14:57:59 +1030

    Fixed ajax login not working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,23 @@
 Devise.setup do |config|
   config.secret_key = 'Temporarily created to fix the devise install error'
+
+  # ==> Scopes configuration
+  # Turn scoped views on. Before rendering "sessions/new", it will first check for
+  # "users/sessions/new". It's turned off by default because it's slower if you
+  # are using only default views.
   config.scoped_views = true
+
+  # If http headers should be returned for AJAX requests. True by default.
+  config.http_authenticatable_on_xhr = false
+
+  # ==> Navigation configuration
+  # Lists the formats that should be treated as navigational. Formats like
+  # :html, should redirect to the sign in page when the user does not have
+  # access, but formats like :xml or :json, should return 401.
+  #
+  # If you have any extra navigational formats, like :iphone or :mobile, you
+  # should add them to the navigational formats lists.
+  #
+  # The "*/*" below is required to match Internet Explorer requests.
+  config.navigational_formats = ['*/*', :html, :json]
 end
